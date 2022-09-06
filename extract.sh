50e9ec1e28edb88f799e283095abd0ce9912fe8a
alberto <alberto@MacBookAlberto.local>
2022-09-06 13:13:12 +0200

    modif

--- extract.sh
+++ extract.sh
@@ -3,17 +3,17 @@
 i=0
 for path in `find . -type f -name loteformat.xml -print`
 do
-	i=i+1
+	i=$((i+1))
 	cd `dirname $path`
-	sed -n "/<Bonos/,/<\/Bonos/p" loteformat.xml > bonos.xml
-	sed -n "/<Comisiones/,/<\/Comisiones/p" loteformat.xml > comisiones.xml
-	sed -n "/<Otros/,/<\/Otros/p" loteformat.xml > otros.xml
-	sed -n "/<Participaciones/,/<\/Participaciones/p" loteformat.xml > participaciones.xml
-	sed -n "/<Premios/,/<\/Premios/p" loteformat.xml > premios.xml
-	sed -n "/<Retiradas/,/<\/Retiradas/p" loteformat.xml > retidadas.xml
+	#sed -n "/<Bonos/,/<\/Bonos/p" loteformat.xml > bonos.xml
+	#sed -n "/<Comision/,/<\/Comision/p" loteformat.xml > comisiones.xml
+	#sed -n "/<Otros/,/<\/Otros/p" loteformat.xml > otros.xml
+	#sed -n "/<Participacion/,/<\/Participacion/p" loteformat.xml > participaciones.xml
+	#sed -n "/<Premios/,/<\/Premios/p" loteformat.xml > premios.xml
+	#sed -n "/<Retiradas/,/<\/Retiradas/p" loteformat.xml > retidadas.xml
 	cd ..
 	#echo $path
 done
 
-echo $i " directories processed"
+echo $i "directories processed."
 
